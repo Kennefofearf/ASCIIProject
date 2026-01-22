11ec47e9be9d9fbff9885a04f499a271e92f23ae
Kennefofearf <kpeterson344@gmail.com>
2026-01-22 18:16:08 -0500

    Created basic cardinal movement for "@"

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import curses
+import time
 from curses import wrapper
 
 
 def gamestart(stdscr):
     curses.cbreak()
     curses.noecho()
+    curses.curs_set(0)
+    stdscr.keypad(True)
     stdscr.clear()
 
-    stdscr.addch(10, 40, "@")
-    stdscr.getch()
+    playery, playerx = 10, 10
 
+    while True:
+        stdscr.clear()
+        player = stdscr.addch(playery, playerx, "@")
+        stdscr.refresh()
+
+        key = stdscr.getch()
+
+        if key == ord("q"):
+            break
+        elif key == ord("a"):
+            playerx -= 1
+        elif key == ord("d"):
+            playerx += 1
+        elif key == ord("w"):
+            playery -= 1
+        elif key == ord("s"):
+            playery += 1
 
 wrapper(gamestart)
