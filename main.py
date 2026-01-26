fe3e73bd168463ef9e202f7f6e16da6e015abbb4
Kennefofearf <kpeterson344@gmail.com>
2026-01-26 09:21:01 -0500

    Created a monster module to store all monster functions and info, Also spawned a monster on the map

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import curses
+import random
 from curses import wrapper
 import player_module
 from player_module import Player
+import monster_module
+from monster_module import GiantAnt
 
 
 def gamestart(stdscr):
@@ -12,13 +15,22 @@ def gamestart(stdscr):
 
     stdscr.clear()
 
+    random_inty, random_intx = random.randint(1, 20), random.randint(1, 100)
+    random_timer = random.randint(3, 15)
+    random_movement = random.randint(1, 10)
+    random_direction = random.randint(-1, 1)
+
     player = Player("Koe", "@", 50, 10, 3)
     player.position = [20, 55]
+
+    giant_ant = GiantAnt("Giant Ant", "A", 12, 5, 1)
+    giant_ant.position = [random_inty, random_intx]
 
     while True:
         stdscr.clear()
         stdscr.border("#", "#", "#", "#", "O", "O", "O", "O")
         stdscr.addch(player.position[0], player.position[1], player.icon)
+        stdscr.addch(giant_ant.position[0], giant_ant.position[1], giant_ant.icon)
         stdscr.refresh()
 
         key = stdscr.getch()
@@ -33,5 +45,16 @@ def gamestart(stdscr):
             player.move(-1, 0)
         elif key == ord("s"):
             player.move(1, 0)
+
+    # while True:
+    #     # Monster movement
+    #     anty, antx = 0, 0
+    #     if random_movement <= 5:
+    #         anty = random_direction
+    #         giant_ant.move(anty, antx, random_timer)
+    #     elif random_movement > 5:
+    #         antx = random_direction
+    #         giant_ant.move(anty, antx, random_timer)
+
 
 wrapper(gamestart)
---
+++ monster_module.py
@@ -0,0 +1,18 @@
+import curses
+import time
+
+class GiantAnt:
+    def __init__(self, name, icon, hp, st, df):
+        self.name = "Giant Ant"
+        self.icon = "A"
+        self.hp = hp
+        self.st = st
+        self.df = df
+        self.position = [0, 0]
+
+    def move(self, py, px, movetimer):
+        self.position[0] += py
+        self.position[1] -= px
+        time.sleep(movetimer)
+
+
