2ea3005456ad85fbec18dbe23c28013abf1890bc
Kennefofearf <kpeterson344@gmail.com>
2026-03-08 14:45:32 -0400

    Added weapons.py with some assumed variables

---
+++ weapons.py
@@ -0,0 +1,29 @@
+import curses
+import random
+from player_module import Player
+
+class Weapons:
+
+    def __init__(self, name, st, df, max_hp, min_dmg, max_dmg, xp, common_skills, lvl):
+        self.name = name
+        self.st = st
+        self.df = df
+        self.max_hp = max_hp
+        self.min_dmg = min_dmg
+        self.max_dmg = max_dmg
+        self.xp = xp
+        self.common_skills = common_skills
+        self.lvl = lvl
+
+    def of_balance(self):
+        self.name = "of Balance"
+        Player.max_hp += 5
+        Player.st += 5
+        Player.df += 5
+        self.min_dmg += random.choice[0, 1, 2]
+        self.max_dmg += random.choice[0, 1, 2]
+        tier_1_common_skills = []
+        row_1 = []
+        row_2 = []
+        row_3 = []
+
