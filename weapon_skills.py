2a9078d42f31a10e47360bb10875d6b5e6c0743d
Kennefofearf <kpeterson344@gmail.com>
2026-03-09 22:48:55 -0400

    Renamed some equipment files and added some parameters to weapons.py

--- weapon_skills.py
+++ weapon_skills.py
@@ -3,9 +3,11 @@ from player_module import Player
 
 class Skill:
 
-    def __init__(self, name, tooltip, max_points, effect, enabled):
+    def __init__(self, name, tooltip, max_points, effect, enabled, points_invested, stat):
         self.name = name
         self.tooltip = tooltip
         self.max_points = max_points
         self.effect = effect
         self.enabled = False
+        self.points_invested = points_invested
+        self.stat = stat
--- weapons.py
+++ weapons.py
@@ -4,13 +4,14 @@ from player_module import Player
 
 class Weapons:
 
-    def __init__(self, name, min_dmg, max_dmg, xp, skills, lvl, capstone_skill, skill_slots):
+    def __init__(self, name, min_dmg, max_dmg, xp, skill_slots, lvl, base_stats, affix, affix_stats):
         self.name = name
         self.min_dmg = min_dmg
         self.max_dmg = max_dmg
+        self.base_stats = base_stats
+        self.affix = affix
+        self.affix_stats = affix_stats
         self.xp = xp
-        self.skills = skills
-        self.capstone_skill = capstone_skill
         self.skill_slots = skill_slots
         self.lvl = lvl
 
