9f796e0120f5bb785c496af624ea38846bbf017b
Kennefofearf <kpeterson344@gmail.com>
2026-03-12 15:49:45 -0400

    Added more weapons skills

--- game/data/weapon_skills_data.py
+++ game/data/weapon_skills_data.py
@@ -19,5 +19,19 @@ COMMON_SKILLS = {
                 "max_points": 5,
                 "effect": None,
                 "stats": {"max_hp": 3, "st": 0, "df": 0}
+            },
+            "Cautious": {
+                "name": "Cautious",
+                "tooltip": "You always think before acting. Raises your Max HP by 2 & DEF by 1.",
+                "max_points": 5,
+                "effect": None,
+                "stats": {"max_hp": 2, "st": 0, "df": 1}
+            },
+            "Reckless": {
+                "name": "Reckless",
+                "tooltip": "You love to charge in without a plan. Raised STR by 3 & decreases DEF by 1.",
+                "max_points": 3,
+                "effect": None,
+                "stats": {"max_hp": 0, "st": 3, "df": -1}
             }
         }
