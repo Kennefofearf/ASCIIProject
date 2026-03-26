a3c6692a0899d4a2a706aae769721b140e702eaa
Kennefofearf <kpeterson344@gmail.com>
2026-03-25 23:22:37 -0400

    Added properties to weapon_abilities.py

--- game/data/weapon_abilities.py
+++ game/data/weapon_abilities.py
@@ -1,17 +1,16 @@
 class Weapon_Abilities:
 
-    def __init__(self, name, tooltip, buff, debuff, damage, points_spent, max_points, gains_per_point, effect):
+    def __init__(self, name, tooltip, points_spent, max_points, gains_per_point):
         self.name = name
         self.tooltip = tooltip
         self.cooldown = 0
-        self.buff = buff
-        self.debuff = debuff
-        self.damage = damage
-        self.effect = effect
+        self.buff = None
+        self.debuff = None
+        self.damage = None
+        self.effect = None
         self.points_spent = points_spent
         self.max_points = max_points
         self.gains_per_point = gains_per_point
-        self.last_tick_time = 0
 
     @property
     def name(self):
