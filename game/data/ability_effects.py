ff1d7cfa17412a5bb2d735c2614961ceedb5f676
Kennefofearf <kpeterson344@gmail.com>
2026-04-04 10:27:09 -0400

    Added an additional effect to ability_effects.py

--- game/data/ability_effects.py
+++ game/data/ability_effects.py
@@ -4,5 +4,11 @@ EFFECTS = {
         "type": "dot",
         "stacking": "refresh",
         "description": "Damage over time"
+    },
+    "st_up": {
+            "name": "Adrenaline",
+            "type": "buff",
+            "stacking": "refresh",
+            "description": "Temporarily raises strength by 5."
     }
 }
