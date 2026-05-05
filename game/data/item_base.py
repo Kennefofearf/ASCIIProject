fd05fcf28ed490b23d60f81847c34068f3c939d1
Kennefofearf <kpeterson344@gmail.com>
2026-05-04 22:17:38 -0400

    Created a create_item_base function in item_base.py

---
+++ game/data/item_base.py
@@ -0,0 +1,15 @@
+def create_item_base():
+    return {
+        "id": None,
+        "name": "",
+        "type": "",
+        "slot": "",
+        "base": None,
+        "rarity": None,
+        "item_lvl": 1,
+        "rolled_stats": {},
+        "base_stats": {},
+        "abilities": [],
+        "affixes": [],
+        "tags": []
+    }
