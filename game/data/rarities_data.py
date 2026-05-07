fceed8eb10dc1af78f731c52a41f199e34e75599
Kennefofearf <kpeterson344@gmail.com>
2026-05-06 20:07:20 -0400

    Created systems/item_drop_generator.py and data/rarities_data.py. Made a dictionary with basic rarity data inside rarities_data.py

---
+++ game/data/rarities_data.py
@@ -0,0 +1,28 @@
+RARITIES = {
+    "normal": {
+        "name": "Normal",
+        "affix_type": "Uncommon_and_rare",
+        "affix_count": 1
+    },
+    "uncommon": {
+            "name": "Uncommon",
+            "affix_type": "Uncommon_and_rare",
+            "affix_count": 2
+    },
+    "rare": {
+            "name": "Rare",
+            "affix_type": "Rare",
+            "affix_count": 1
+    },
+    "epic": {
+            "name": "Epic",
+            "affix_type": "Epic",
+            "affix_count": 1
+    },
+    "legendary": {
+                "name": "Legendary",
+                "affix_type": "Legendary",
+                "affix_count": 1
+    }
+
+}
---
+++ game/systems/item_drop_generator.py
@@ -0,0 +1,6 @@
+import random
+
+from loot_generator import generate_item
+
+dropRNG = random.uniform(1, 10000)
+
