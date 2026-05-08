e82bd8009e606f3c9ad373ead5725abfba91a055
Kennefofearf <kpeterson344@gmail.com>
2026-05-07 23:57:22 -0400

    Added more imports to loot_generator.py. Also added some lightweight stat rolling functions. Commented stat rolling out for now as they may not be used.

--- game/systems/loot_generator.py
+++ game/systems/loot_generator.py
@@ -1,5 +1,25 @@
+import random
+
 from game.data.weapons_data import WEAPONS
 from game.data.item_base import create_item_base
+from game.data.affix_data import UNCOMMON_AFFIXES
+from game.data.rarities_data import RARITIES
+
+# def roll_value(value):
+#     if isinstance(value, list):
+#         return random.randint(value[0], value[1])
+#
+#     return value
+#
+# def roll_stats(stat_block, stat_multi):
+#     rolled = {}
+#
+#     for stat_name, value in stat_block.items():
+#         rolled_value = roll_value(value)
+#         rolled[stat_name] = max(0, int(rolled_value * stat_multi))
+#
+#     return rolled
+
 
 def generate_item(base_id):
     base = WEAPONS[base_id]
