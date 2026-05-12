c32bbd38b517d7dee1d99dd336d0eda6176b9c38
Kennefofearf <kpeterson344@gmail.com>
2026-05-11 22:31:13 -0400

    Added "type" to the affixes definitions to define if they are prefixes or suffixes.

--- game/data/affix_data.py
+++ game/data/affix_data.py
@@ -3,6 +3,7 @@ import random
 UNCOMMON_AFFIXES = {
         "of Balance": {
             "name": "of Balance",
+            "type": "suffix",
             "affix_stats": {"max_hp": 5, "st": 5, "df": 5},
             "min_dmg": random.choice([0, 1, 2]),
             "max_dmg": random.choice([0, 1, 2]),
@@ -10,6 +11,7 @@ UNCOMMON_AFFIXES = {
         },
         "of Minor Constitution": {
             "name": "of Minor Constitution",
+            "type": "suffix",
             "affix_stats": {"max_hp": 15, "st": 0, "df": 0},
             "min_dmg": random.choice([0, 1, 2]),
             "max_dmg": random.choice([0, 1, 2]),
@@ -17,6 +19,7 @@ UNCOMMON_AFFIXES = {
         },
         "of Perseverance": {
             "name": "of Perseverance",
+            "type": "suffix",
             "affix_stats": {"max_hp": 9, "st": 0, "df": 7},
             "min_dmg": random.choice([0, 1, 2]),
             "max_dmg": random.choice([0, 1, 2]),
@@ -24,6 +27,7 @@ UNCOMMON_AFFIXES = {
         },
         "of the Rotund Fool": {
             "name": "of the Rotund Fool",
+            "type": "suffix",
             "affix_stats": {"max_hp": 20, "st": -10, "df": -7},
             "min_dmg": random.choice([0, 1, 2]),
             "max_dmg": random.choice([0, 1, 2]),
