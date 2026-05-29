535cb004113faf671590fd3980d7af7f32e9c90d
Kennefofearf <kpeterson344@gmail.com>
2026-05-29 00:38:34 -0400

    Inventory Screen
    feat: Add a separate window to display clicked item descriptions

--- game/UI/inventory_screen.py
+++ game/UI/inventory_screen.py
@@ -13,12 +13,15 @@ def open_inventory_window(stdscr, player):
         margin_x = int(stdscr_x * 0.3)
         height = stdscr_y - (margin_y * 2)
         width = stdscr_x - (margin_x * 2)
+
+        dw_margin_y = int(stdscr_y * 0.3)
+        dw_margin_x = int(stdscr_x * 0.6)
         inventory_window = curses.newwin(height, width, margin_y, margin_x)
         inventory_window.box()
         inventory = player.inventory
 
         inventory_window.addstr(1, (int(width / 2) - 11), f"Inventory ({len(inventory)})")
-        inventory_window.addstr(2, 1, str(inventory)[:width - 2])
+        #inventory_window.addstr(2, 1, str(inventory)[:width - 2])
 
         item_rows = {}
 
@@ -30,6 +33,10 @@ def open_inventory_window(stdscr, player):
             item_rows[margin_y + row] = item
 
         if selected_item:
+
+            item_description_window = curses.newwin(height, width, dw_margin_y, dw_margin_x)
+            item_description_window.box()
+
             detail_x = width // 2
 
             affixes = selected_item.get("affixes", [])
@@ -39,22 +46,24 @@ def open_inventory_window(stdscr, player):
             for affix_id in affixes:
                 affix_data = UNCOMMON_AFFIXES[affix_id]
 
-                inventory_window.addstr(row, detail_x, selected_item["name"])
+                item_description_window.addstr(row, detail_x, selected_item["name"])
                 row += 1
-                inventory_window.addstr(row, detail_x, f"{selected_item['min_dmg'] + affix_data['min_dmg']}"
+                item_description_window.addstr(row, detail_x, f"{selected_item['min_dmg'] + affix_data['min_dmg']}"
                                                        f" - {selected_item['max_dmg'] + affix_data['max_dmg']}")
                 row += 1
 
                 affix_stats = affix_data.get("affix_stats", {})
 
                 for stat, value in affix_stats.items():
-                    inventory_window.addstr(row, detail_x, f"{stat.upper()}: {value}")
+                    item_description_window.addstr(row, detail_x, f"{stat.upper()}: {value}")
                     row += 1
+                item_description_window.refresh()
 
             if not affixes:
-                inventory_window.addstr(row, detail_x, selected_item["name"])
+                item_description_window.addstr(row, detail_x, selected_item["name"])
                 row += 1
-                inventory_window.addstr(row, detail_x, f"{selected_item['min_dmg']} - {selected_item['max_dmg']}")
+                item_description_window.addstr(row, detail_x, f"{selected_item['min_dmg']} - {selected_item['max_dmg']}")
+                item_description_window.refresh()
 
         inventory_window.refresh()
         key = stdscr.getch()
