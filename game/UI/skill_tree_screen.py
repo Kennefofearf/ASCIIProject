672e1b2ab886cce1fc8bc64309c75570eda3140b
Kennefofearf <kpeterson344@gmail.com>
2026-06-16 20:01:38 -0400

    skill_tree_data
    feat: Add a function to draw the item title in the skill tree window

--- game/UI/skill_tree_screen.py
+++ game/UI/skill_tree_screen.py
@@ -1,4 +1,5 @@
 import curses
+from systems.loot_generator import get_rarity_color
 
 def draw_node(window, y, x, label=""):
     window.addstr(y, x, "_____")
@@ -6,7 +7,17 @@ def draw_node(window, y, x, label=""):
     window.addstr(y+ 2, x, f"|{label:^5}|")
     window.addstr(y + 3, x, "|_____|")
 
+def draw_item_name(window, item, width):
+    name = item["name"]
+    item_color = get_rarity_color(item)
+    xp = item["xp"]
+    max_xp = item["max_xp"]
+
+    title = f"{name}    {xp}/{max_xp}"
+    window.addstr(1, max(1, (width - len(title)) / 2), title, curses.color_pair(item_color))
+
 def open_skill_tree(stdscr, selected_item):
+    selected_slot = 0
 
     while True:
         stdscr_y, stdscr_x = stdscr.getmaxyx()
