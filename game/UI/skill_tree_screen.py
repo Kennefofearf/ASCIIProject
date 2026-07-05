b069fe77ce6fe30c5305434ac22e4a98acc683b7
Kennefofearf <kpeterson344@gmail.com>
2026-07-05 17:03:24 -0400

    skill_tree_screen.py
    feat: Add scrolling to the skill tree window

--- game/UI/skill_tree_screen.py
+++ game/UI/skill_tree_screen.py
@@ -17,7 +17,10 @@ def draw_node(window, x, y, label, node, is_selected=False):
     node_height = 5
     node_width = 7
 
-    if y + node_height >= height or x + node_width >= width:
+    if y < 1 or y + node_height >= height:
+        return
+
+    if x < 1 or x + node_width >= width:
         return
 
     color = curses.A_REVERSE if is_selected else curses.A_NORMAL
@@ -40,7 +43,7 @@ def draw_item_name(window, item, width):
     title = f"{name}    {xp}/{max_xp}"
     window.addstr(1, max(1, (width - len(title)) / 2), title, item_color)
 
-def draw_skill_tree_nodes(window, item, selected_slot):
+def draw_skill_tree_nodes(window, item, selected_slot, scroll):
     layout = item["skill_tree"]["layout"]
 
     for slot_index, position in enumerate(layout["slots"]):
@@ -48,6 +51,7 @@ def draw_skill_tree_nodes(window, item, selected_slot):
             continue
 
         y, x = position
+        x -= scroll
 
         node = item["skill_tree"]["nodes"].get(slot_index)
 
@@ -65,7 +69,10 @@ def draw_skill_tree_nodes(window, item, selected_slot):
 
 
 def open_skill_tree(stdscr, selected_item):
+    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
+
     selected_slot = 0
+    scroll_y = 0
 
     while True:
         stdscr_y, stdscr_x = stdscr.getmaxyx()
@@ -75,14 +82,10 @@ def open_skill_tree(stdscr, selected_item):
         tree_x = int(stdscr_x * 0.55)
 
         skill_tree_window = curses.newwin(height, tree_width, start_y, tree_x)
-        skill_tree_window.scrollok(True)
-        skill_tree_window.idlok(True)
         skill_tree_window.box()
 
-        height, width = skill_tree_window.getmaxyx()
-
         # draw_item_name(skill_tree_window, selected_item, width)
-        draw_skill_tree_nodes(skill_tree_window, selected_item, selected_slot)
+        draw_skill_tree_nodes(skill_tree_window, selected_item, selected_slot, scroll_y)
         skill_tree_window.refresh()
 
         key = stdscr.getch()
@@ -91,4 +94,12 @@ def open_skill_tree(stdscr, selected_item):
             skill_tree_window.erase()
             skill_tree_window.refresh()
             break
+        elif key == curses.KEY_MOUSE:
+            _, mx, my, _, bstate, = curses.getmouse()
+
+            if bstate & curses.BUTTON4_PRESSED:
+                scroll_y = max(0, scroll_y - 2)
+
+            elif bstate & curses.BUTTON5_PRESSED:
+                scroll_y += 2
 
