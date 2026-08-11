702b63c677f4e724a4f256b5efce5a07ec98359f
Kennefofearf <kpeterson344@gmail.com>
2026-08-11 17:35:10 -0400

    item_module.py
    fix: Change all variable values to match the create_item_base function

--- game/item_module.py
+++ game/item_module.py
@@ -1,19 +1,18 @@
 class Item:
 
-    def __init__(self, id, name, type, rarity, base_stats, attack_cooldown, item_lvl, xp, max_xp, lvl, max_lvl,
-                 skill_points, abilities, affixes, tags):
-        self.id = id
-        self.name = name
-        self.type = type
-        self.rarity = rarity
-        self.base_stats = base_stats
-        self.attack_cooldown = attack_cooldown
-        self.item_lvl = item_lvl
-        self.xp = xp
-        self.max_xp = max_xp
-        self.lvl = lvl
-        self.max_lvl = max_lvl
-        self.skill_points = skill_points
+    def __init__(self):
+
+        self.id = None
+        self.name = ""
+        self.type = ""
+        self.rarity = None
+        self.base_stats = {}
+        self.item_lvl = 1
+        self.xp = 0
+        self.max_xp = 100
+        self.lvl = 0
+        self.max_lvl = 10
+        self.skill_points = 0
         self.abilities = []
-        self.affixes = {}
+        self.affixes = []
         self.tags = []
