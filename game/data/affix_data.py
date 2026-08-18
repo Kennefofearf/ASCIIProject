ab69094921151668d35a7d79e05d900faa6245be
Kennefofearf <kpeterson344@gmail.com>
2026-08-18 15:47:05 -0400

    ref: Finish affix refactor

--- game/data/affix_data.py
+++ game/data/affix_data.py
@@ -10,31 +10,28 @@ UNCOMMON_AFFIXES = {
             max_dmg=random.choice([0, 1, 2]),
             item_type=["weapon", "armor"]
         ),
-        # "of_minor_constitution": {
-        #     "name": "of Minor Constitution",
-        #     "type": "suffix",
-        #     "affix_stats": {"max_hp": 15, "st": 0, "df": 0},
-        #     "min_dmg": random.choice([0, 1, 2]),
-        #     "max_dmg": random.choice([0, 1, 2]),
-        #     "slots": 7,
-        #     "item_type": ["weapon", "armor"]
-        # },
-        # "of_perseverance": {
-        #     "name": "of Perseverance",
-        #     "type": "suffix",
-        #     "affix_stats": {"max_hp": 9, "st": 0, "df": 7},
-        #     "min_dmg": random.choice([0, 1, 2]),
-        #     "max_dmg": random.choice([0, 1, 2]),
-        #     "slots": 7,
-        #     "item_type": ["weapon", "armor"]
-        # },
-        # "of_the_rotund_fool": {
-        #     "name": "of the Rotund Fool",
-        #     "type": "suffix",
-        #     "affix_stats": {"max_hp": 20, "st": -10, "df": -7},
-        #     "min_dmg": random.choice([0, 1, 2]),
-        #     "max_dmg": random.choice([0, 1, 2]),
-        #     "slots": 7,
-        #     "item_type": ["weapon", "armor"]
-        # }
+        "of_minor_constitution": Affix(
+            name="of Minor Constitution",
+            affix_type="suffix",
+            affix_stats={"max_hp": 15, "st": 0, "df": 0},
+            min_dmg=random.choice([0, 1, 2]),
+            max_dmg=random.choice([0, 1, 2]),
+            item_type=["weapon", "armor"]
+        ),
+        "of_perseverance": Affix(
+            name="of Perseverance",
+            affix_type="suffix",
+            affix_stats={"max_hp": 9, "st": 0, "df": 7},
+            min_dmg=random.choice([0, 1, 2]),
+            max_dmg=random.choice([0, 1, 2]),
+            item_type=["weapon", "armor"]
+        ),
+        "of_the_rotund_fool": Affix(
+            name="of the Rotund Fool",
+            affix_type="suffix",
+            affix_stats={"max_hp": 20, "st": -10, "df": -7},
+            min_dmg=random.choice([0, 1, 2]),
+            max_dmg=random.choice([0, 1, 2]),
+            item_type=["weapon", "armor"]
+        )
     }
